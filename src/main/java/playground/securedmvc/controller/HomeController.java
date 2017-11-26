f30c75395a86f7f1be0dc169c55a135fb7313220
Zoltán Domahidi <domahidizoltan@gmail.com>
2017-11-26 15:01:10 +0100

    add Thymeleaf templates with i18n support

---
+++ src/main/java/playground/securedmvc/controller/HomeController.java
@@ -0,0 +1,17 @@
+package playground.securedmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public ModelAndView welcome() {
+        ModelAndView index = new ModelAndView("index");
+        index.addObject("name", "MVC");
+        return index;
+    }
+
+}
